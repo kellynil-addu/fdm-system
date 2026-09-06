813e0201fa13df913e84751fc538d1d2ba91d980
Kelly Neil Ang <angkellyn14@gmail.com>
2026-09-06 17:53:42 +0800

    Add local IP to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // cacheComponents disabled to support dynamic data fetching in pages
+  allowedDevOrigins: ['127.0.0.1']
 };
 
 export default nextConfig;
