c250358bcaa3761797429347aa4872fd69690b06
Kelly Neil Ang <angkellyn14@gmail.com>
2026-09-06 16:32:10 +0800

    Modify middleware to only check auth in 'dashboard' route

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -48,17 +48,13 @@ export async function updateSession(request: NextRequest) {
   const user = data?.claims;
 
   if (
-    request.nextUrl.pathname !== "/" &&
     !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/opengraph-image") &&
-    !request.nextUrl.pathname.startsWith("/twitter-image")
+    request.nextUrl.pathname.startsWith("/dashboard")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/login";
-    url.searchParams.set("next", request.nextUrl.pathname);
+    url.searchParams.set("next", request.nextUrl.pathname + request.nextUrl.search);
     return NextResponse.redirect(url);
   }
 
